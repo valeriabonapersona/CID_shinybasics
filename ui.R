d1006d041279f1ed8ec8bdf285813e2c73535991
valeriabonapersona <valeria.bonapersona@gmail.com>
2022-05-31 12:38:24 +0200

    var explanation

--- ui.R
+++ ui.R
@@ -10,7 +10,10 @@ fluidPage(
   sidebarLayout(
     sidebarPanel(
       h3("Select data of interest"),
-      fct_input("diet", "Diet", diet_groups)
+      fct_input("diet", "Diet", diet_groups) 
+          # "diet" is the id for input (ie, it becomes input$diet)
+          # "Diet" is the label that will appear on the app interface
+          # diet_groups is a variable containing the choices. This was defined in src/general.R
       ## do here the same for sex
       ## fct_slider_input() for "time"
       ## all fct_input() need to be separated by a comma.
